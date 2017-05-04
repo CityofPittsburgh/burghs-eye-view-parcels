b6508b3f4ff6a786145ad449937d2b607593a258
mcercone <maxwell.cercone@pittsburghpa.gov>
2017-05-04 16:29:56 -0400

    clean up api calls

--- pmapcleanr.R
+++ pmapcleanr.R
@@ -15,15 +15,16 @@ dollarsComma <- function(x){
   paste0("$", x)
 }
 
+this_year <- as.Date(format(Sys.Date(), format="%Y-01-01"))
+
+
 ##Query Ckan API for Property Assessment Data
-query <- "https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20from%20%22518b583f-7cc8-4f60-94d0-174cc98310dc%22%20WHERE%20%22SCHOOLCODE%22%20LIKE%20%2747%27&limit=9999999"
+query <- "https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20%22PARID%22%2C%22PROPERTYHOUSENUM%22%2C%22PROPERTYFRACTION%22%2C%22PROPERTYADDRESS%22%2C%22PROPERTYZIP%22%2C%22MUNIDESC%22%2C%22TAXDESC%22%2C%22CLASSDESC%22%2C%22OWNERDESC%22%2C%22USEDESC%22%2C%22HOMESTEADFLAG%22%2C%22COUNTYLAND%22%2C%22COUNTYBUILDING%22%2C%22COUNTYTOTAL%22%2C%22SALEPRICE%22%2C%22SALEDATE%22%2C%22YEARBLT%22%20from%20%22518b583f-7cc8-4f60-94d0-174cc98310dc%22%20WHERE%20%22SCHOOLCODE%22%20LIKE%20%2747%27&limit=9999999"
 getdata <- GET(url=query)
 assessment <- jsonlite::fromJSON(content(getdata, "text"))
 assessment <- assessment$result$records
-assessed <- subset(assessment, select = c("PARID", "PROPERTYHOUSENUM", "PROPERTYFRACTION", "PROPERTYADDRESS", "PROPERTYZIP", "MUNIDESC", "TAXDESC", "CLASSDESC", "OWNERDESC",
-                                          "USEDESC", "HOMESTEADFLAG", "COUNTYLAND", "COUNTYBUILDING", "COUNTYTOTAL", "SALEPRICE", "SALEDATE", "YEARBLT"))
-assessed$MUNIDESC <- as.factor(assessed$MUNIDESC)
-assessed <- subset(assessed, MUNIDESC != "Mt. Oliver  ")
+assessment$MUNIDESC <- as.factor(assessment$MUNIDESC)
+assessment <- subset(assessment, MUNIDESC != "Mt. Oliver  ")
 
 ##Query Ckan API for Delinquent
 delinquent.query <- "https://data.wprdc.org/api/action/datastore_search?resource_id=ed0d1550-c300-4114-865c-82dc7c23235b&limit=99999"
@@ -53,14 +54,14 @@ abatement$abatement <- TRUE
 liens <- read.csv("liens.csv")
 
 ##PLI Violation
-v.query <- "https://data.wprdc.org/api/action/datastore_search?resource_id=4e5374be-1a88-47f7-afee-6a79317019b4&limit=99999"
+v.query <- "https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20%22PARCEL%22%2C%22VIOLATION%22%2C%22INSPECTION_RESULT%22%2C%22CORRECTIVE_ACTION%22%2C%22LOCATION%22%2C%22INSPECTION_DATE%22%20from%20%224e5374be-1a88-47f7-afee-6a79317019b4%22&limit=9999999"
 getv <- GET(url=v.query, add_headers(Authorization = "74b409d8-0f6f-439a-8a97-7796b9a0fc8b"))
 violations <- jsonlite::fromJSON(content(getv, "text"))
 violations <- violations$result$records
-violations <- subset(violations, select = c("PARCEL", "VIOLATION", "INSPECTION_RESULT", "CORRECTIVE_ACTION", "LOCATION", "INSPECTION_DATE"))
-
+violations <- subset(violations, INSPECTION_DATE >= this_year)
 
 ##Neighborhood
+system('python pghhoods.py')
 load.nhood <- fromJSON("./pghnhoods.txt")
 load.nhood <- load.nhood$result$records
 load.nhood$geo_name_nhood <- as.factor(load.nhood$geo_name_nhood)
@@ -74,9 +75,10 @@ hood_list <- tolower(hood_list)
 write_json(hood_list, "hoodlist.json")
 
 ##Merge all datasets together
-all.property <- merge(assessed, delq, by.x = "PARID", by.y = "pin", all.x = TRUE)
+all.property <- merge(assessment, delq, by.x = "PARID", by.y = "pin", all.x = TRUE)
 all.property <- merge(all.property, ownership, by.x = "PARID", "pin", all.x = TRUE)
 all.property <- merge(all.property, abatement, by.x = "PARID", "pin", all.x = TRUE)
+all.property <- merge(all.property, violations, by.x = "PARID", "PARCEL", all.x = TRUE)
 all.property$ADDRESS <- paste(all.property$PROPERTYHOUSENUM, all.property$PROPERTYADDRESS)
 all.property$SALEDATE <- gsub("-", "/", all.property$SALEDATE)
 all.property$SALEDATE <- as.Date(all.property$SALEDATE, "%m/%d/%Y")
@@ -95,6 +97,10 @@ all.property$program_name[is.na(all.property$program_name)] <- "No Abatement"
 all.property$abatement[is.na(all.property$abatement)] <- FALSE
 all.property$delq[is.na(all.property$delq)] <- FALSE
 all.property$cityowned[is.na(all.property$cityowned)] <- FALSE
+all.property$INSPECTION_DATE <- as.Date(all.property$INSPECTION_DATE)
+all.property$VIOLATION[is.na(all.property$VIOLATION)] <- "No Violation"
+all.property$INSPECTION_RESULT[is.na(all.property$INSPECTION_RESULT)] <- ""
+
 
 #Set tooltip fields with dollars and commas
 all.property$tt_land <- dollarsComma(all.property$COUNTYLAND)
@@ -114,11 +120,11 @@ all.property$colorval <- ifelse(all.property$cityowned == TRUE, "#ffff33", all.p
 all.property$colorval <- ifelse(all.property$delq == TRUE, "#e41a1c", all.property$colorval)
 
 ##Function that binds geoJSON data to dataframe
-pinjson <- function(x){
-  baseUrl <- "http://tools.wprdc.org/geoservice/parcels_in/pittsburgh_neighborhood/"
-  endUrl <- x$nhood
+pinjson <- function(data){
+  baseURL <- "http://tools.wprdc.org/geoservice/parcels_in/pittsburgh_neighborhood/"
+  endUrl <- data$nhood
   count <- 1
-  for (i in x$PARID){
+  for (i in data$PARID){
     r <- GET(baseURL, endUrl)
     c <- fromJSON(content(r, "text", encoding = "ISO-8859-1"))
     c.1 <- as.data.frame(c$features)
